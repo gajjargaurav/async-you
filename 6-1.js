20b12ce8f0c0fe5bafeb2ce81826c159e8d87ef7
gajjargaurav <gajjargaurav@gmail.com>
2015-01-20 15:18:04 +0000

    exercise 6 complete with request module

---
+++ 6-1.js
@@ -0,0 +1,15 @@
+var http = require('http')
+	, wrapper = require('./httpwrapper')
+	, async = require('async')
+	, request = require('request');
+var arr = ['one', 'two', 'three'];
+async.reduce(arr, 0, function(memo, item, callback){
+	var url = process.argv[2].toString().trim() + '?number=' + item;
+	 request(url, function(err,response, body){
+            if(err) callback(err);
+            callback(null, memo + Number(body));
+        });
+},function(err, result){
+  if (err) return console.log(err);
+  console.log(result);
+});
